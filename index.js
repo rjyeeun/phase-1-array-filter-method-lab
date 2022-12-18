7dba73a3cd1da5a77c186e0085e8074ab5e42a22
Yeeun Jung <rjyeeun@gmail.com>
2022-12-17 23:26:27 -0500

    commit tree 1310ad13e02566adb73c2b7939441c6262114d2e

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+
+function findMatching(drivers,string) {
+    return drivers.filter(function(driver)
+    {return driver.toLowerCase() === string.toLowerCase()})
+    }
+    
+
+function fuzzyMatch(drivers,string) {
+    return drivers.filter(function(driver)
+    {return driver.toLowerCase().slice(0,string.length) === string.toLowerCase()}) }
+    
+    
+function matchName(drivers,string) {
+    return drivers.filter(function(driver)
+    {return driver.name === string
+    
+    })}
